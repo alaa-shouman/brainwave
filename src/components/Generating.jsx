91dadc2d5c288c5ab46faad44be78149c25389c2
alaa-shouman <alaa.shouman@st.ul.edu.lb>
2024-07-07 08:22:24 +0300

    brainwave 1

---
+++ src/components/Generating.jsx
@@ -0,0 +1,13 @@
+import { loading } from "../assets"
+
+const Generating = () => {
+  return (
+    <div>
+      <img className="w-5 h-5 mr-4"
+        src={loading}
+      />
+    </div>
+  )
+}
+
+export default Generating
